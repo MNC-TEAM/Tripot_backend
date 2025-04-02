fe01663949c1ad993d535a5ee44ee0c6f7e10d64
TheperZ <ckdgh7884@gmail.com>
2025-04-02 17:01:32 +0900

    공지사항 게시, 수정 검증 코드 추가

--- module-domain/src/main/java/com/junior/dto/notice/UpdateNoticeDto.java
+++ module-domain/src/main/java/com/junior/dto/notice/UpdateNoticeDto.java
@@ -1,8 +1,16 @@
 package com.junior.dto.notice;
 
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record UpdateNoticeDto(
+        @Size(max = 255, message = "너무 긴 제목입니다.")
+        @NotNull(message = "제목은 필수입니다.")
         String title,
+
+        @Size(max = 65535, message = "너무 긴 내용입니다.")
+        @NotNull(message = "내용은 필수입니다.")
         String content
 ) {
 }
