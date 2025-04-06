575d6a56e8a9bfdc65b90b2169a50e3e888b300a
TheperZ <ckdgh7884@gmail.com>
2025-04-07 00:32:02 +0900

    축제 저장로직 검증코드 추가

--- module-api/src/main/java/com/junior/controller/api/FestivalApi.java
+++ module-api/src/main/java/com/junior/controller/api/FestivalApi.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,8 +22,8 @@ public interface FestivalApi {
             @ApiResponse(responseCode = "201", description = "축제 데이터 저장 성공"),
             @ApiResponse(responseCode = "500", description = "축제 데이터 저장 실패")
     })
-    public ResponseEntity<CommonResponse<Object>> saveFestival(@RequestParam(value = "eventStartDate") String eventStartDate,
-                                                               @RequestParam(value = "eventEndDate", required = false, defaultValue = "") String eventEndDate);
+    public ResponseEntity<CommonResponse<Object>> saveFestival(@Pattern(regexp = "^[0-9]{8}", message = "잘못 된 날짜 형식입니다.") @RequestParam(value = "eventStartDate") String eventStartDate,
+                                                               @Pattern(regexp = "^[0-9]{8}|^$", message = "잘못 된 날짜 형식입니다.") @RequestParam(value = "eventEndDate", required = false, defaultValue = "") String eventEndDate);
 
 
     @Operation(summary = "축제 도시 개수", description = "시/도 별 축제의 개수를 응답합니다.")
