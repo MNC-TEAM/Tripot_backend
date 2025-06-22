98737c584e3fbcab9d3a54db4f35bb50e86ca25b
heeseong <donald010@naver.com>
2025-06-22 16:27:01 +0900

    csv를 통한 팝업 이벤트 생성 기능 추가

--- module-api/src/main/java/com/junior/service/popUpEvent/PopUpEventService.java
+++ module-api/src/main/java/com/junior/service/popUpEvent/PopUpEventService.java
@@ -20,7 +20,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -45,6 +50,47 @@ public class PopUpEventService {
         PopUpEvent popUpEvent = PopUpEvent.from(createNewPopUpEventDto);
 
         popUpEventRepository.save(popUpEvent);
+    }
+
+    @Transactional
+    public void createEventsFromCsv(UserPrincipal userPrincipal, MultipartFile file) {
+        Member findMember = userPrincipal.getMember();
+
+        if (findMember.getRole() != MemberRole.ADMIN) {
+            throw new PermissionException(StatusCode.PERMISSION_ERROR);
+        }
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8))) {
+            String line;
+            boolean isFirst = true;
+
+            while ((line = reader.readLine()) != null) {
+                if (isFirst) {
+                    isFirst = false;
+                    continue;
+                }
+
+                String[] tokens = line.split(",");
+
+                CreateNewPopUpEventDto dto = CreateNewPopUpEventDto.builder()
+                        .eventName(tokens[0])
+                        .eventUrl(tokens[1])
+                        .location(tokens[2])
+                        .city(tokens[3])
+                        .latitude(Double.parseDouble(tokens[4]))
+                        .longitude(Double.parseDouble(tokens[5]))
+                        .startDate(LocalDateTime.parse(tokens[6] + "T00:00:00"))
+                        .endDate((LocalDateTime.parse(tokens[7] + "T23:59:59")))
+                        .build();
+
+                PopUpEvent event = PopUpEvent.from(dto);
+                popUpEventRepository.save(event);
+
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Transactional
